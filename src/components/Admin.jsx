6095a3d7165f298bb3c04a7bdb494b699cb606fa
SathishMadesh <s747272946@gmail.com>
2023-12-05 14:19:28 +0530

    link api

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,9 +1,74 @@
 import React from "react";
+import { useState } from "react";
+import Axios from 'axios';
+
 
 let Admin = () => {
 
+    // For form submit event
+    let[submit,setSubmit] = useState(false);
+
+    // Required fields for product
+    let [product,setProduct] = useState({name:'',discription:'',imgurl:'',price:'',quantity:''});
+
+    // For Form events update the values
+    let updateHandler = (event) => {
+        setProduct({...product,
+            [event.target.name]:event.target.value});
+    };
+
+    // imageHandler get the image as a file and convert into url and update the value
+    let imgHandler = (event) => {
+        let imageFile = event.target.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(imageFile);
+        reader.addEventListener("load",()=>{
+            if (reader.result){
+                setProduct({
+                    ...product,imgurl:reader.result
+                })
+            }
+            else {
+                console.log("Unable to convert")
+            }
+        })
+    };
+
+    let submitHandler = (event) => {
+        event.preventDefault();
+        Axios.post('http://127.0.0.1:8000/products/',{product},{headers:{
+            'Content-Type': 'application/json',
+            'x-csrftoken': 'your-csrf-token',
+          }})
+        .then((response)=>{
+            setSubmit(true);
+        })
+        .catch((error)=>{
+            console.log("Can not able to push",error.message)
+        })
+    }
+
     return <div>
-        <h1>About</h1>
+        <pre>{JSON.stringify(product)}</pre>
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <input type="text" placeholder="Product Name" name="name" onChange={updateHandler} className="form-control"/>
+            </div>
+            <div className="form-group">
+                <input type="text" placeholder="Product Discription" name="discription" onChange={updateHandler} className="form-control"/>
+            </div>
+            <div className="form-group">
+                <input type="file" placeholder="Image" name="imgurl" onChange={imgHandler} className="form-control"/>
+            </div>
+            <div className="form-group">
+                <input type="text" placeholder="Price" name="price" onChange={updateHandler} className="form-control"/>
+            </div>
+            <div className="form-group">
+                <input type="text" placeholder="QTY" name="quantity" onChange={updateHandler} className="form-control"/>
+            </div>
+
+            <input type='submit' value="upload" className='btn btn-warning'/>
+        </form>
     </div>
 }
 export default Admin
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 let Home = ( )=>{
 
+    let [products,setProducts] = useState([]);
+
+    useEffect(()=>{
+        Axios.get('http://127.0.0.1:8000/products.json')
+        .then((response)=>{
+            setProducts(response.data);
+        })
+        .catch((error)=>{
+            console.error("Error fetching data:", error);
+        });
+    },[]);
+
     return <div>
-        <h1>Home</h1>
+        <pre>{JSON.stringify(products)}</pre>
+        {
+            products.map((product,id)=>{
+                return <div className="cred" key={id}>
+                    <div className="card-head">
+                        <img src={product.imgurl} alt="image" />
+                    </div>
+                    <div className="card-body">
+                        <h4>{product.name}</h4>
+                        <h4>{product.price}</h4>
+                    </div>
+                </div>
+            })
+        }
     </div>
 }
 
