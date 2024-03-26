7323851ae5f4ebfdb73dbe59f069b465b60c646b
SathishMadesh <s747272946@gmail.com>
2024-03-26 19:48:48 +0530

    update

--- src/components/StdList.jsx
+++ src/components/StdList.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 function StdList(props) {
+
+  
+
   return (
     <div>
       <ul>
         {props.stdList.map((stdName, index) => (
-          <li key={index}>{stdName}</li>
+          <li key={index}>{stdName} <button onClick={deleteHandler}>Delete</button> </li>
         ))}
       </ul>
     </div>
